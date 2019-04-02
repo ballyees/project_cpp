9ec434a44a2b6d2929091eb7d26f6da5e68cd82c
ballyees <shoulderip@gmail.com>
2019-04-02 17:02:03 +0700

    a

--- connect.bat
+++
@@ -1,4 +0,0 @@
-echo off
-mysql -u root -p123456 < sql.txt
-cls
-exit
--- connectForO.bat
+++
@@ -1,8 +0,0 @@
-echo off
-cd C:\ProgramData\MySQL\MySQL Server 8.0\Uploads
-del querySQL.txt
-cd C:\Users\MSI-PS42-OEM\Desktop\project_compro\shop
-mysql -u root -p123456 < sql.txt
-echo all | copy "C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\querySQL.txt" "C:\Users\MSI-PS42-OEM\Desktop\project_compro\shop"
-cls
-exit
