a5276cab8fbcf2bccc517a78bc2cff42ad5fa512
chryne <46591138+chryne@users.noreply.github.com>
2019-04-26 00:40:42 +0700

    สมการยังไม่เสร็จ

--- Pie/pie.cpp
+++ Pie/pie.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <cstdlib>
 #include "sqlForPie.cpp"
@@ -15,49 +16,40 @@ int main()
 
 
     initgraph(&gd , &gm , "C:\\TC\\BGI");
-    setcolor(MAGENTA);
-    rectangle(0,40,639,450);
-    settextstyle(SANS_SERIF_FONT,HORIZ_DIR,2);
+    //setcolor(MAGENTA);
+    //rectangle(0,40,639,450);
+    settextstyle(SANS_SERIF_FONT,HORIZ_DIR,4);
     setcolor(WHITE);
-    outtextxy(270,10,"PIE CHART");
+    outtextxy(225,25,"PIE CHART");
 
     midx = getmaxx()/2;
-    midy = getmaxy()/2;
+    midy = getmaxy()/2+20;
 
     float sum =0;
     vector<bill> test = getdataChart(10,"2019");
     for(int i=0; i<test.size(); i++){
-        cout << test[i].detailP.name << ":" << test[i].detailP.price << endl;
         sum+=test[i].detailP.price;
     }
-    /*
-    ifstream input("simple.txt");
-    string d;
-    vector <float> data;
-    vector <string> name_data;
-    float sum = 0;
-    int n = 0;
-    while(getline(input,d)){
-        char format[] = "%[^ :] %f";
-        char name[100];
-        float cost;
-        sscanf(d.c_str(),format,name,&cost);
-        data.push_back(cost);
-        name_data.push_back(name);
-        sum += cost;
-        n ++;
-        cout<<cost<<endl;
-    }
-    cout<<data[0];*/
-    //int n = 6; //input data number
-    //float data[6]={55,34,21,3,2,4},sum=0;//input data
     float pend=0,pstart=0;
+    settextstyle(SANS_SERIF_FONT,HORIZ_DIR,2);
     for (int i=0; i<test.size(); i++){
         pend += (test[i].detailP.price/sum)*360;
         setfillstyle(3,(i/2,i*2,i+2));
-        pieslice(midx , midy , pstart , pend , 100);
+        pieslice(midx , midy , pstart , pend , 150);
+        /*settextstyle(SANS_SERIF_FONT,HORIZ_DIR,2);
+        outtextxy(midx*cos((pend-pstart)/2), midy *sin((pend-pstart)/2),&test[0].detailP.name[0]);
+        if((pend-pstart)/2 >= 0 && (pend-pstart)/2 <90){
+            outtextxy(midx*cos((pend-pstart)/2), midy *sin((pend-pstart)/2),&test[i].detailP.name[0]);
+        }else if ((pend-pstart)/2 >= 90 && (pend-pstart)/2 <180){
+            outtextxy(midx*cos((pend-pstart)/2), midy *sin((pend-pstart)/2),&test[i].detailP.name[0]);
+        }else if ((pend-pstart)/2 >= 180 && (pend-pstart)/2 <270){
+            outtextxy(midx*cos((pend-pstart)/2), midy *sin((pend-pstart)/2),&test[i].detailP.name[0]);
+        }else{
+            outtextxy(midx*cos((pend-pstart)/2), midy *sin((pend-pstart)/2),&test[i].detailP.name[0]);
+        }*/
         pstart=pend;
     }
+
 
     getch();
     return 0;
