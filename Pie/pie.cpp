84f7c2723d5be85bcab7f7aaed6ad09b394b9542
chryne <46591138+chryne@users.noreply.github.com>
2019-04-26 01:12:10 +0700

    Update pie.cpp

--- Pie/pie.cpp
+++ Pie/pie.cpp
@@ -16,13 +16,11 @@ int main()
 
 
     initgraph(&gd , &gm , "C:\\TC\\BGI");
-    //setcolor(MAGENTA);
-    //rectangle(0,40,639,450);
     settextstyle(SANS_SERIF_FONT,HORIZ_DIR,4);
     setcolor(WHITE);
     outtextxy(225,25,"PIE CHART");
 
-    midx = getmaxx()/2;
+    midx = getmaxx()/2+80;
     midy = getmaxy()/2+20;
 
     float sum =0;
@@ -31,22 +29,13 @@ int main()
         sum+=test[i].detailP.price;
     }
     float pend=0,pstart=0;
-    settextstyle(SANS_SERIF_FONT,HORIZ_DIR,2);
     for (int i=0; i<test.size(); i++){
         pend += (test[i].detailP.price/sum)*360;
         setfillstyle(3,(i/2,i*2,i+2));
         pieslice(midx , midy , pstart , pend , 150);
-        /*settextstyle(SANS_SERIF_FONT,HORIZ_DIR,2);
-        outtextxy(midx*cos((pend-pstart)/2), midy *sin((pend-pstart)/2),&test[0].detailP.name[0]);
-        if((pend-pstart)/2 >= 0 && (pend-pstart)/2 <90){
-            outtextxy(midx*cos((pend-pstart)/2), midy *sin((pend-pstart)/2),&test[i].detailP.name[0]);
-        }else if ((pend-pstart)/2 >= 90 && (pend-pstart)/2 <180){
-            outtextxy(midx*cos((pend-pstart)/2), midy *sin((pend-pstart)/2),&test[i].detailP.name[0]);
-        }else if ((pend-pstart)/2 >= 180 && (pend-pstart)/2 <270){
-            outtextxy(midx*cos((pend-pstart)/2), midy *sin((pend-pstart)/2),&test[i].detailP.name[0]);
-        }else{
-            outtextxy(midx*cos((pend-pstart)/2), midy *sin((pend-pstart)/2),&test[i].detailP.name[0]);
-        }*/
+        pieslice(25,150 + (i*25), 0 , 360 , 10);
+        settextstyle(SANS_SERIF_FONT,HORIZ_DIR,2);
+        outtextxy(40, 138 + (i*25),&test[i].detailP.name[0]);
         pstart=pend;
     }
 
